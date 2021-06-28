fdcca2db9a0a33b392c57f6a03921a4e94de6bed
CARLOS GARCIA <mrgarcia7@gmail.com>
2021-06-27 23:57:40 -0500

    Create employee controller

---
+++ Redarbor/Redarbor.Api/Controllers/RedarborController.cs
@@ -0,0 +1,64 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Redarbor.Api.Responses;
+using Redarbor.Core.DTOs.Request;
+using Redarbor.Core.Entities;
+using Redarbor.Core.Interfaces;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Redarbor.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]    
+    public class RedarborController : ControllerBase
+    {
+        private readonly IEmployeeService service;
+        private readonly IMapper mapper;
+
+        public RedarborController(IEmployeeService service, IMapper mapper)
+        {            
+            this.service = service;
+            this.mapper = mapper;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddEmployee(EmployeeRequestDto employee)
+        {
+            var mappedEmployee = mapper.Map<Employee>(employee);
+            var addedEmployee = await service.Add(mappedEmployee);
+            var response = new ApiResponse<Employee>(addedEmployee);
+
+            return Ok(response);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetEmployees()
+        {            
+            var employees = await service.GetAll();            
+            var response = new ApiResponse<IReadOnlyList<Employee>>(employees);
+
+            return Ok(response);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateEmployee(int id, EmployeeRequestDto employee)
+        {
+            var mappedSong = mapper.Map<Employee>(employee);
+            mappedSong.Id = id;
+            var addedEmployee = await service.Update(mappedSong);            
+            var response = new ApiResponse<Employee>(addedEmployee);
+
+            return Ok(response);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteEmployee(int id)
+        {            
+            await service.Delete(id);            
+            var response = new ApiResponse<string>("Success delete");
+
+            return Ok(response);
+        }
+    }
+}
